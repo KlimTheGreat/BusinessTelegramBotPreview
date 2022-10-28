439c1af0ca171e7b38cf9b88366d3fc67434acc9
Klim <ya.klim@list.ru>
2022-10-29 00:46:38 +0300

    error handler

--- Code/Main.py
+++ Code/Main.py
@@ -130,18 +130,21 @@ async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
 
 async def data_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_id = update.message.from_user.id
-    user_name = await UsersTable.get_user_name(user_id)
-    user_phone = await UsersTable.get_user_phone(user_id)
-    user_email = await UsersTable.get_user_email(user_id)
-    await update.message.reply_text(
-        "Твои данные: \n\n"
-        f"Имя: *{user_name}*\n"
-        f"Телефон: *{user_phone}*\n"
-        f"Email: *{user_email}*\n\n",
-        parse_mode=ParseMode.MARKDOWN,
-        reply_markup=ReplyKeyboardRemove()
-    )
-    await help_handler(update, context)
+    if user_id in await UsersTable.get_all_user_ids():
+        user_name = await UsersTable.get_user_name(user_id)
+        user_phone = await UsersTable.get_user_phone(user_id)
+        user_email = await UsersTable.get_user_email(user_id)
+        await update.message.reply_text(
+            "Твои данные: \n\n"
+            f"Имя: *{user_name}*\n"
+            f"Телефон: *{user_phone}*\n"
+            f"Email: *{user_email}*\n\n",
+            parse_mode=ParseMode.MARKDOWN,
+            reply_markup=ReplyKeyboardRemove()
+        )
+        await help_handler(update, context)
+    else:
+        await start_handler(update, context)
     return ConversationHandler.END
 
 
@@ -431,6 +434,50 @@ async def successful_payment_callback(update: Update, context: ContextTypes.DEFA
     )
 
 
+async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the alarm message."""
+    job = context.job
+    text = f"Beep! {job.data} seconds are over!"
+    await context.bot.send_message(job.chat_id, text)
+
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Add a job to the queue."""
+    chat_id = update.effective_message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("Sorry we can not go back to future!")
+            return
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+        text = "Timer successfully set!"
+        if job_removed:
+            text += " Old one was removed."
+        await update.effective_message.reply_text(text)
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Usage: /set <seconds>")
+
+
+async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    await update.message.reply_text(text)
+
+
 # run the bot
 def main() -> None:
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).read_timeout(7).get_updates_read_timeout(42).build()
@@ -491,6 +538,10 @@ def main() -> None:
     application.add_handler(
         MessageHandler(filters.SUCCESSFUL_PAYMENT, successful_payment_callback)  # successful payment handler
     )
+
+    # timer
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
 
     # help
     application.add_handler(CommandHandler("help", help_handler))  # on command
